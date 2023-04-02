504e2aa42c4669c85de0b9a4addd2b7fe2772985
Mark Arzangulyan <Mark.arzangulyan@yandex.ru>
2023-04-02 23:08:11 +0300

    Обновление исходников морфологии

---
+++ Bizin_morfology/convex_numeric.py
@@ -0,0 +1,69 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import functions as f
+import heapq
+
+
+# parameters settings
+period = 48
+delta = 3
+
+
+# data import
+df = pd.read_excel('/Users/arzangulyan/Documents/Научка/Vesna2022/Programming/Attempt_1/Bizin_morfology/SO2_vse_urovni_Vyetnam_1000.xlsx')
+print("data import complete")
+y = np.array(df.S1CO2)[11:200]
+x = np.array(df.index)[11:200]
+size = len(x)
+x = x-x[0]
+
+
+form_parameters = np.linspace(0, size, size*2//period + 1, dtype=int)
+inner_cycle = 2 * delta + 1
+outer_cycle = len(form_parameters) - 2
+for i in range(1, len(form_parameters)-1):
+    h = []
+    outer_done = i - 1
+
+    for infl_p in range(form_parameters[i-1] + period//2 - delta, form_parameters[i-1] + period//2 + delta + 1):
+        infl_p = min(infl_p, size)
+        inner_done = infl_p - (form_parameters[i-1] + period//2 - delta)
+        print(round((outer_done * inner_cycle + inner_done) / (outer_cycle * inner_cycle) * 100, 2), '%')
+
+        form_parameters[i] = infl_p
+
+        A = f.convex_constraints_matrix(form_parameters)
+        app = f.numeric_sol(y, A)
+        heapq.heappush(h, (np.linalg.norm(y - app), infl_p))
+
+    form_parameters[i] = h[0][1]
+print(100, '%')
+
+A = f.convex_constraints_matrix(form_parameters)
+APPROXIMATION = f.numeric_sol(y, A)
+
+infl_p = form_parameters[1:-1]
+periods_1 = [infl_p[i+2] - infl_p[i] for i in range(len(infl_p)-2)]
+periods = np.array([(periods_1[i] + periods_1[i+1])/2 for i in range(0, len(periods_1)-1, 2)])
+avg_period = periods.mean()
+
+# graphs
+fig, ax1 = plt.subplots(1)
+ax1.plot(x, y, 'ro', markersize=2, label='experimental data')
+ax1.plot(x, APPROXIMATION, 'b', linewidth=1, label='result of morphological filtering')
+ax1.plot(x[infl_p], APPROXIMATION[infl_p], 'go', markersize=5, label='inflection points')
+ax1.set_xlabel("Time (1 tick = 30 min)")
+ax1.set_ylabel("CO2 concentration")
+legend1 = ax1.legend(loc='best', shadow=True, fontsize='large')
+
+plt.show()
+
+'''
+# ax2.plot(
+#     x, y-APPROXIMATION, 'g', linewidth=1, label="residual series"
+#     )
+# ax2.set_xlabel("Time (1 tick = 30 min)")
+# ax2.set_ylabel("CO2 concentration")
+# legend2 = ax2.legend(loc='best', shadow=True, fontsize='large')
+'''
--- Bizin_morfology/functions.py
+++ Bizin_morfology/functions.py
@@ -1,299 +1,72 @@
-"""
-functions for the approximation
-"""
 import numpy as np
+import cvxpy as cp
 
 
-def checkconvexup(q):
-
-    for i in range(1, len(q)-1):
-        if (2*q[i] < q[i-1]+q[i+1]) and (abs(2*q[i]-q[i-1]-q[i+1]) > (1e-10)):
-            return False
-    return True
-
-
-def checkconvexdown(q):
-
-    q = np.negative(q)
-    return checkconvexup(q)
-
-
-def mono_incr(q):
-    if (q[0:-1] > q[1:]).any():
-        return False
-    else:
-        return True
-
-
-def mono_decr(q):
-    if (q[0:-1] < q[1:]).any():
-        return False
-    else:
-        return True
-
-
-def leastsq(f):
-    n = len(f)
-    x = np.linspace(1, n, n)
-    A = np.vstack([x, np.ones(n)]).T
-    a, b = np.linalg.lstsq(A, f, rcond=None)[0]
-
-    return np.array(a*x + b)
-
-
-def mono_incr_leastsq(f):
-    n = len(f)
-    x = np.linspace(1, n, n)
-    A = np.vstack([x, np.ones(n)]).T
-    a, b = np.linalg.lstsq(A, f, rcond=None)[0]
-    if a >= 0:
-        return np.array(a*x + b)
-    else:
-        popt = np.polyfit(x, f, 0)
-        return np.polyval(popt, x)
-
-
-def mono_decr_leastsq(f):
-    n = len(f)
-    x = np.linspace(1, n, n)
-    A = np.vstack([x, np.ones(n)]).T
-    a, b = np.linalg.lstsq(A, f, rcond=None)[0]
-    if a <= 0:
-        return np.array(a*x + b)
-    else:
-        popt = np.polyfit(x, f, 0)
-        return np.polyval(popt, x)
-
-
-def projconvup(f, init, final, points):
-    curr_segment = f[init:final]
-    segment_len = len(curr_segment)
-    if points[init][final] != 0 or segment_len <= 3:
-        return build_proj_conv_up(f, init, final, points)
-    else:
-        for i in range(1, segment_len):
-            if points[init][init+i] != 0 or i <= 3:
-                leftproj = build_proj_conv_up(f, init, init+i, points)
+def convex_constraints_matrix(form_parameters):
+    size = form_parameters[-1]
+    for col_ind in range(len(form_parameters) - 1):
+        if (col_ind % 2 == 0):
+            if form_parameters[col_ind] == 0:
+                A = np.hstack([[-1, 1], np.zeros(size - 2)])
+                for j in range(form_parameters[1] - 2):
+                    m = [-0.5, 1, -0.5]
+                    tmp = np.hstack([np.zeros(j), m, np.zeros(size - j - 3)])
+                    A = np.vstack([A, tmp])
             else:
-                leftproj = projconvup(f, init, init+i, points)
-
-            if segment_len - i <= 3 or points[init+i][final] != 0:
-                rightproj = build_proj_conv_up(f, init+i, final, points)
-            else:
-                rightproj = projconvup(f, init+i, final, points)
-
-            q = np.append(leftproj, rightproj)
-            if (checkconvexup(q)):
-                points[init][final] = init + i
-                return q
-
-        points[init][final] = -1
-        return leastsq(curr_segment)
-
-
-def projconvdown(f, init, final, points):
-    curr_segment = f[init:final]
-    segment_len = len(curr_segment)
-    if points[init][final] != 0 or segment_len <= 3:
-        return build_proj_conv_up(f, init, final, points)
-    else:
-        for i in range(1, segment_len):
-            if i <= 3 or points[init][init+i] != 0:
-                leftproj = build_proj_conv_down(f, init, init+i, points)
-            else:
-                leftproj = projconvdown(f, init, init+i, points)
-
-            if segment_len - i <= 3 or points[init+i][final] != 0:
-                rightproj = build_proj_conv_down(f, init+i, final, points)
-            else:
-                rightproj = projconvdown(f, init+i, final, points)
-
-            q = np.append(leftproj, rightproj)
-            if (checkconvexdown(q)):
-                points[init][final] = init + i
-                return q
-
-        points[init][final] = -1
-        return leastsq(curr_segment)
-
-
-def proj_mono_incr(f, init, final, points):
-    curr_segment = f[init:final]
-    segment_len = len(curr_segment)
-    if points[init][final] != 0 or segment_len <= 3:
-        return build_proj_mono_incr(f, init, final, points)
-    else:
-        for i in range(1, segment_len):
-            if i <= 3 or points[init][init+i] != 0:
-                leftproj = build_proj_mono_incr(f, init, init+i, points)
-            else:
-                leftproj = proj_mono_incr(f, init, init+i, points)
-
-            if segment_len - i <= 3 or points[init+i][final] != 0:
-                rightproj = build_proj_mono_incr(f, init+i, final, points)
-            else:
-                rightproj = proj_mono_incr(f, init+i, final, points)
-
-            q = np.append(leftproj, rightproj)
-            if (mono_incr(q)):
-                points[init][final] = init + i
-                return q
-
-        points[init][final] = -1
-        return mono_incr_leastsq(curr_segment)
-
-
-def proj_mono_decr(f, init, final, points):
-    curr_segment = f[init:final]
-    segment_len = len(curr_segment)
-    if points[init][final] != 0 or segment_len <= 3:
-        return build_proj_mono_decr(f, init, final, points)
-    else:
-        for i in range(1, segment_len):
-            if i <= 3 or points[init][init+i] != 0:
-                leftproj = build_proj_mono_decr(f, init, init+i, points)
-            else:
-                leftproj = proj_mono_decr(f, init, init+i, points)
-
-            if segment_len - i <= 3 or points[init+i][final] != 0:
-                rightproj = build_proj_mono_decr(f, init+i, final, points)
-            else:
-                rightproj = proj_mono_decr(f, init+i, final, points)
-
-            q = np.append(leftproj, rightproj)
-            if (mono_decr(q)):
-                points[init][final] = init + i
-                return q
-
-        points[init][final] = -1
-        return mono_decr_leastsq(curr_segment)
-
-
-def is_ext(f, ind):
-    if len(f) < 3:
-        return False
-    elif (f[ind-1] < f[ind] and f[ind+1] < f[ind]) or (f[ind-1] > f[ind] and f[ind+1] > f[ind]):
-        return True
-    else:
-        return False
-
-
-def is_max(f, ind):
-    if ind == 0 or ind == len(f)-1:
-        return False
-    elif (f[ind-1] < f[ind] and f[ind+1] < f[ind]):
-        return True
-    else:
-        return False
-
-
-def is_min(f, ind):
-    if ind == 0 or ind == len(f)-1:
-        return False
-    elif (f[ind-1] > f[ind] and f[ind+1] > f[ind]):
-        return True
-    else:
-        return False
-
-
-def build_proj_conv_up(y, init, final, points):
-    if (points[init][final] == -1):
-        return leastsq(y[init:final])
-    elif (final - init == 1) or (final - init == 2):
-        return y[init:final]
-    elif (final - init == 3):
-        if checkconvexup(y[init:final]):
-            return y[init:final]
+                m = [-1, 1]
+                tmp = np.hstack([np.zeros(form_parameters[col_ind] - 1), m, np.zeros(size - form_parameters[col_ind] - 1)])
+                A = np.vstack([A, tmp])
+                tmp = np.hstack([np.zeros(form_parameters[col_ind]), m, np.zeros(size - form_parameters[col_ind] - 2)])
+                A = np.vstack([A, tmp])
+                for j in range(form_parameters[col_ind], form_parameters[col_ind+1] - 2):
+                    m = [-0.5, 1, -0.5]
+                    tmp = np.hstack([np.zeros(j), m, np.zeros(size - j - 3)])
+                    A = np.vstack([A, tmp])
         else:
-            return leastsq(y[init:final])
+            m = [1, -1]
+            tmp = np.hstack([np.zeros(form_parameters[col_ind] - 1), m, np.zeros(size - form_parameters[col_ind] - 1)])
+            A = np.vstack([A, tmp])
+            tmp = np.hstack([np.zeros(form_parameters[col_ind]), m, np.zeros(size - form_parameters[col_ind] - 2)])
+            A = np.vstack([A, tmp])
+            for j in range(form_parameters[col_ind], form_parameters[col_ind+1] - 2):
+                m = [0.5, -1, 0.5]
+                tmp = np.hstack([np.zeros(j), m, np.zeros(size - j - 3)])
+                A = np.vstack([A, tmp])
+    return A
+
+
+def monotonous_constraints_matrix(form_parameters):
+    size = form_parameters[-1]
+    A = np.zeros(size)
+    for col_ind in range(len(form_parameters) - 2):
+        if (col_ind % 2 == 0):
+            m = [-1, 1]
+            for j in range(form_parameters[col_ind], form_parameters[col_ind + 1]):
+                tmp = np.hstack([np.zeros(j), m, np.zeros(size - j - 2)])
+                A = np.vstack([A, tmp])
+        else:
+            m = [1, -1]
+            for j in range(form_parameters[col_ind], form_parameters[col_ind + 1]):
+                tmp = np.hstack([np.zeros(j), m, np.zeros(size - j - 2)])
+                A = np.vstack([A, tmp])
+    if len(form_parameters) % 2 == 0:
+        m = [-1, 1]
+        for j in range(form_parameters[-2], form_parameters[-1] - 1):
+            tmp = np.hstack([np.zeros(j), m, np.zeros(size - j - 2)])
+            A = np.vstack([A, tmp])
     else:
-        return np.append(build_proj_conv_up(y, init, points[init][final], points),
-                         build_proj_conv_up(y, points[init][final], final, points))
+        m = [1, -1]
+        for j in range(form_parameters[-2], form_parameters[-1] - 1):
+            tmp = np.hstack([np.zeros(j), m, np.zeros(size - j - 2)])
+            A = np.vstack([A, tmp])
+    return A[1:]
 
 
-def build_proj_conv_down(y, init, final, points):
-    if (points[init][final] == -1):
-        return leastsq(y[init:final])
-    elif (final - init == 1) or (final - init == 2):
-        return y[init:final]
-    elif (final - init == 3):
-        if checkconvexdown(y[init:final]):
-            return y[init:final]
-        else:
-            return leastsq(y[init:final])
-    else:
-        return np.append(build_proj_conv_down(y, init, points[init][final], points),
-                         build_proj_conv_down(y, points[init][final], final, points))
-
-
-def build_proj_mono_incr(y, init, final, points):
-    if (points[init][final] == -1):
-        return mono_incr_leastsq(y[init:final])
-    elif (final - init == 1) or (final - init == 2):
-        return y[init:final]
-    elif (final - init == 3):
-        if mono_incr(y[init:final]):
-            return y[init:final]
-        else:
-            return mono_incr_leastsq(y[init:final])
-    else:
-        return np.append(build_proj_mono_incr(y, init, points[init][final], points),
-                         build_proj_mono_incr(y, points[init][final], final, points))
-
-
-def build_proj_mono_decr(y, init, final, points):
-    if (points[init][final] == -1):
-        return mono_decr_leastsq(y[init:final])
-    elif (final - init == 1) or (final - init == 2):
-        return y[init:final]
-    elif (final - init == 3):
-        if mono_decr(y[init:final]):
-            return y[init:final]
-        else:
-            return mono_decr_leastsq(y[init:final])
-    else:
-        return np.append(build_proj_mono_decr(y, init, points[init][final], points),
-                         build_proj_mono_decr(y, points[init][final], final, points))
-
-
-def dif(q, f):
-    return (sum(i*i for i in (q-f)))
-
-
-def find_closest(array, value, curr_index):
-    for i in range(curr_index+1, len(array)):
-        if array[i][0] == value:
-            return i
-    return 'err'
-
-
-def find_next(evolution, i):
-    for j in range(i+1, len(evolution)-1):
-        if evolution[j] != -1:
-            return j
-    return None
-
-
-def find_min_delts(delts):
-    min_ind = delts[0][0]
-    min_val = delts[0][1]
-    for i in range(1, len(delts)):
-        if delts[i][1] < min_val:
-            min_ind = delts[i][0]
-            min_val = delts[i][1]
-    return min_ind
-
-
-def optimal_point(delts, priority):
-    if priority == 1:
-        if delts[0]:
-            return find_min_delts(delts[0])
-        else:
-            return find_min_delts(delts[1])
-    elif priority == 0:
-        if delts[0] and delts[1]:
-            return find_min_delts(delts[0]+delts[1])
-        elif delts[0]:
-            return find_min_delts(delts[0])
-        else:
-            return find_min_delts(delts[1])
+def numeric_sol(f, A):
+    C = np.zeros(A.shape[0])
+    X = cp.Variable(A.shape[1])
+    constraints = [A@X >= C]
+    obj = cp.Minimize(cp.norm(X-f))
+    prob = cp.Problem(obj, constraints)
+    prob.solve()
+    return X.value
