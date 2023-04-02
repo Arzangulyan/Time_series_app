be43d6475ec63cf3868e36b847d1988a1cc0bf83
Mark Arzangulyan <Mark.arzangulyan@yandex.ru>
2023-04-02 16:26:53 +0300

    Исправлена еще ошибка с пустым рядом

--- TimeSeriesApp/Time_Series_App.py
+++ TimeSeriesApp/Time_Series_App.py
@@ -49,7 +49,7 @@ def df_chart_display_iloc(df, start, end, data_col_iloc):
     st.line_chart(df.iloc[start:end, data_col_iloc])
 
 if "final_dataframe" not in st.session_state:
-    st.session_state.final_dataframe = None
+    st.session_state.final_dataframe = pd.DataFrame(None)
 
 
 # trigonometric_args = [S_1_coef, S_1_freq, S_2_coef, S_2_freq, S_3_coef, S_3_freq, C_1_coef, C_1_freq, C_2_coef, C_2_freq, C_3_coef, C_3_freq]
