57d1cf8971dca74a62e67b90f3901906226b8ee1
Mark Arzangulyan <15670678+Arzangulyan@users.noreply.github.com>
2024-08-14 12:58:17 +0300

    replace freqs with periods

--- config.py
+++ config.py
@@ -2,17 +2,17 @@ STATIONARITY_THRESHOLD = 0.05
 DATA_TYPES = ["", "Искусственный ряд", "Загруженный ряд"]
 PARAM_NAMES = [
     "S_1_coef",
-    "S_1_freq",
+    "S_1_period",
     "S_2_coef",
-    "S_2_freq",
+    "S_2_period",
     "S_3_coef",
-    "S_3_freq",
+    "S_3_period",
     "C_1_coef",
-    "C_1_freq",
+    "C_1_period",
     "C_2_coef",
-    "C_2_freq",
+    "C_2_period",
     "C_3_coef",
-    "C_3_freq",
+    "C_3_period",
     "NoiseCoef",
     "TrendSlope",
 ]
