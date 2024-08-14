0e340ed0ae48259f3c3f90f9d6cd9b0c51774006
Mark Arzangulyan <15670678+Arzangulyan@users.noreply.github.com>
2024-08-14 15:41:20 +0300

    fix synthetic generation add up

--- modules/synthetic_data.py
+++ modules/synthetic_data.py
@@ -4,20 +4,21 @@ from .utils import DF_wrapper
 
 from streamlit import write
 
+
 def generate_synthetic_time_series(
     date_range: pd.DatetimeIndex,
     S_1_coef: float = 0.0,
-    S_1_period: float = 1.0,
+    S_1_period: float = 0.0,
     S_2_coef: float = 0.0,
-    S_2_period: float = 1.0,
+    S_2_period: float = 0.0,
     S_3_coef: float = 0.0,
-    S_3_period: float = 1.0,
+    S_3_period: float = 0.0,
     C_1_coef: float = 0.0,
-    C_1_period: float = 1.0,
+    C_1_period: float = 0.0,
     C_2_coef: float = 0.0,
-    C_2_period: float = 1.0,
+    C_2_period: float = 0.0,
     C_3_coef: float = 0.0,
-    C_3_period: float = 1.0,
+    C_3_period: float = 0.0,
     NoiseCoef: float = 0.0,
     TrendSlope: float = 0.0,
 ) -> pd.DataFrame:
@@ -39,20 +40,29 @@ def generate_synthetic_time_series(
     length = len(date_range)
     t = np.linspace(0, length - 1, length)
 
-    sinusoids = (
-        S_1_coef * np.sin(2 * np.pi * t / S_1_period) if S_1_coef != 0 else 0
-        + S_2_coef * np.sin(2 * np.pi * t / S_2_period) if S_2_coef != 0 else 0
-        + S_3_coef * np.sin(2 * np.pi * t / S_3_period) if S_3_coef != 0 else 0
-    )
+    sinusoids = np.zeros(length)
+    if S_1_period != 0:
+        sinusoids += S_1_coef * np.sin(2 * np.pi * t / S_1_period)
+    if S_2_period != 0:
+        sinusoids += S_2_coef * np.sin(2 * np.pi * t / S_2_period)
+    if S_3_period != 0:
+        sinusoids += S_3_coef * np.sin(2 * np.pi * t / S_3_period)
 
-    cosines = (
-        C_1_coef * np.cos(2 * np.pi * t / C_1_period) if C_1_coef != 0 else 0
-        + C_2_coef * np.cos(2 * np.pi * t / C_2_period) if C_2_coef != 0 else 0
-        + C_3_coef * np.cos(2 * np.pi * t / C_3_period) if C_3_coef != 0 else 0
-    )
+    cosines = np.zeros(length)
+    if C_1_period != 0:
+        cosines += C_1_coef * np.cos(2 * np.pi * t / C_1_period)
+    if C_2_period != 0:
+        cosines += C_2_coef * np.cos(2 * np.pi * t / C_2_period)
+    if C_3_period != 0:
+        cosines += C_3_coef * np.cos(2 * np.pi * t / C_3_period)
 
-    trend = TrendSlope * t if TrendSlope != 0 else 0
-    noise = np.random.normal(0, NoiseCoef, length) if NoiseCoef != 0 else 0
+    trend = TrendSlope * t if TrendSlope != 0 else np.zeros(length)
+    noise = (
+        np.random.normal(0, NoiseCoef, length) if NoiseCoef != 0 else np.zeros(length)
+    )
 
     time_series = trend + sinusoids + cosines + noise
+    print(
+        f"S_1_coef: {S_1_coef}, S_2_coef: {S_2_coef}, S_1_period: {S_1_period}, S_2_period: {S_2_period}"
+    )
     return pd.DataFrame({"Время": date_range, "Ряд": time_series})
