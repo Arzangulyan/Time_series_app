d0f45902dee0e54d03412117f1149bd084a9a28c
Mark Arzangulyan <15670678+Arzangulyan@users.noreply.github.com>
2024-08-18 11:15:18 +0300

    rename sin\cos funcs

--- modules/synthetic_data.py
+++ modules/synthetic_data.py
@@ -7,32 +7,32 @@ from streamlit import write
 
 def generate_synthetic_time_series(
     date_range: pd.DatetimeIndex,
-    S_1_coef: float = 0.0,
-    S_1_period: float = 0.0,
-    S_2_coef: float = 0.0,
-    S_2_period: float = 0.0,
-    S_3_coef: float = 0.0,
-    S_3_period: float = 0.0,
-    C_1_coef: float = 0.0,
-    C_1_period: float = 0.0,
-    C_2_coef: float = 0.0,
-    C_2_period: float = 0.0,
-    C_3_coef: float = 0.0,
-    C_3_period: float = 0.0,
-    NoiseCoef: float = 0.0,
-    TrendSlope: float = 0.0,
+    SIN_1_coef: float = 0.0,
+    SIN_1_period: float = 0.0,
+    SIN_2_coef: float = 0.0,
+    SIN_2_period: float = 0.0,
+    SIN_3_coef: float = 0.0,
+    SIN_3_period: float = 0.0,
+    COS_1_coef: float = 0.0,
+    COS_1_period: float = 0.0,
+    COS_2_coef: float = 0.0,
+    COS_2_period: float = 0.0,
+    COS_3_coef: float = 0.0,
+    COS_3_period: float = 0.0,
+    Noise_Coef: float = 0.0,
+    Trend_Slope: float = 0.0,
 ) -> pd.DataFrame:
     """
     Генерирует синтетический временной ряд.
 
     Args:
         date_range (pd.DatetimeIndex): Диапазон дат для временного ряда.
-        S_1_coef, S_2_coef, S_3_coef (float): Коэффициенты амплитуды для синусоид.
-        S_1_period, S_2_period, S_3_period (float): Периоды для синусоид.
-        C_1_coef, C_2_coef, C_3_coef (float): Коэффициенты амплитуды для косинусоид.
-        C_1_period, C_2_period, C_3_period (float): Периоды для косинусоид.
+        SIN_1_coef, SIN_2_coef, SIN_3_coef (float): Коэффициенты амплитуды для синусоид.
+        SIN_1_period, SIN_2_period, SIN_3_period (float): Периоды для синусоид.
+        COS_1_coef, COS_2_coef, COS_3_coef (float): Коэффициенты амплитуды для косинусоид.
+        COS_1_period, COS_2_period, COS_3_period (float): Периоды для косинусоид.
         NoiseCoef (float): Коэффициент шума.
-        TrendSlope (float): Наклон линейного тренда.
+        Trend_Slope (float): Наклон линейного тренда.
 
     Returns:
         pd.DataFrame: DataFrame с синтетическим временным рядом.
@@ -41,24 +41,24 @@ def generate_synthetic_time_series(
     t = np.linspace(0, length - 1, length)
 
     sinusoids = np.zeros(length)
-    if S_1_period != 0:
-        sinusoids += S_1_coef * np.sin(2 * np.pi * t / S_1_period)
-    if S_2_period != 0:
-        sinusoids += S_2_coef * np.sin(2 * np.pi * t / S_2_period)
-    if S_3_period != 0:
-        sinusoids += S_3_coef * np.sin(2 * np.pi * t / S_3_period)
+    if SIN_1_period != 0:
+        sinusoids += SIN_1_coef * np.sin(2 * np.pi * t / SIN_1_period)
+    if SIN_2_period != 0:
+        sinusoids += SIN_2_coef * np.sin(2 * np.pi * t / SIN_2_period)
+    if SIN_3_period != 0:
+        sinusoids += SIN_3_coef * np.sin(2 * np.pi * t / SIN_3_period)
 
     cosines = np.zeros(length)
-    if C_1_period != 0:
-        cosines += C_1_coef * np.cos(2 * np.pi * t / C_1_period)
-    if C_2_period != 0:
-        cosines += C_2_coef * np.cos(2 * np.pi * t / C_2_period)
-    if C_3_period != 0:
-        cosines += C_3_coef * np.cos(2 * np.pi * t / C_3_period)
+    if COS_1_period != 0:
+        cosines += COS_1_coef * np.cos(2 * np.pi * t / COS_1_period)
+    if COS_2_period != 0:
+        cosines += COS_2_coef * np.cos(2 * np.pi * t / COS_2_period)
+    if COS_3_period != 0:
+        cosines += COS_3_coef * np.cos(2 * np.pi * t / COS_3_period)
 
-    trend = TrendSlope * t if TrendSlope != 0 else np.zeros(length)
+    trend = Trend_Slope * t if Trend_Slope != 0 else np.zeros(length)
     noise = (
-        np.random.normal(0, NoiseCoef, length) if NoiseCoef != 0 else np.zeros(length)
+        np.random.normal(0, Noise_Coef, length) if Noise_Coef != 0 else np.zeros(length)
     )
 
     time_series = trend + sinusoids + cosines + noise
